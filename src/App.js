85437c61e10a09fcad8fc13c3eb7926255746ad1
AMANDA DE LISBOA SAUL <02295488060@senacrs.edu.br>
2024-07-12 12:17:13 -0300

    pesquisa

--- src/App.js
+++ src/App.js
@@ -1,25 +1,25 @@
-import logo from './logo.svg';
+import React from 'react';
+import { ThemeProvider, useTheme } from './ThemeContext';
 import './App.css';
 
-function App() {
+const App = () => {
+  const { theme, toggleTheme } = useTheme();
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className={`App ${theme}`}>
+      <h1>Alternador de cores</h1>
+      <button onClick={toggleTheme}>Alterne-os</button>
+      <p>Tema atual: {theme}</p>
     </div>
   );
-}
+};
 
-export default App;
+const AppWrapper = () => {
+  return (
+    <ThemeProvider>
+      <App />
+    </ThemeProvider>
+  );
+};
+
+export default AppWrapper;
